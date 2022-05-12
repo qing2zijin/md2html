1f21694919ee5d443afd5930d9db40725f948d9f
qing2zijin <47149655+qing2zijin@users.noreply.github.com>
2022-05-12 16:27:36 +0800

    Update main.py

--- main.py
+++ main.py
@@ -9,7 +9,6 @@
         5.生成目录，并结束
 '''
 import os, markdown, re, time
-
 # from pathlib import Path
 
 class thread_str(object):
@@ -31,7 +30,7 @@ class Node:  #用于存放数据的结点，搭配列表使用
 
 class md2html(object):
     pages = 0                            #页数
-    articleMax = 30                      #设置每页文章数
+    articleMax = 40                      #设置每页文章数
     post_num = 0                         #文章数量
     is_only2page = False                 #总共只有2页判断
     template_post = None                 #文章模板
@@ -46,6 +45,8 @@ class md2html(object):
         self.base = base
         self.site_name = site_name
         self.arr = []
+        self.site_url = site_UL
+    
     def findAllFile(self):
         for root, ds, fs in os.walk(self.base):
             for f in fs:
@@ -63,7 +64,9 @@ class md2html(object):
               说明：采取的是列表之间对比，列表needthing作为匹配结果，列表tagg作为设置的原始值，
             '''
             needthing = [] #title 0, date 1, tags 2, private 3, topping 4, is_archive 5, keywords 6, description 7
+            
             length = len(self.pattern)
+            
             for k in range(length):
                 matchaddr = re.search(self.pattern[k], raw_Data, re.S)  #循环匹配
                 if matchaddr is not None:           #排查：如果md文件里面没有设置参数
@@ -98,9 +101,9 @@ class md2html(object):
 
     def HTML_url(self, md_addr, parse_to_html=None):
         if parse_to_html == True:
-            return  md_addr.replace("source", "/posts").replace("\\", "/").replace(".md", ".html") 
+            return  md_addr.replace(self.base, "/posts").replace("\\", "/").replace(".md", ".html") 
         else:
-            return  md_addr.replace("source", "posts").replace("\\", "/").replace(".md", ".html")
+            return  md_addr.replace(self.base, "posts").replace("\\", "/").replace(".md", ".html")
         #return ( '%s.html'%(os.path.basename(md_addr).lower().replace('.md', '')) )
         
     def parse_to_HTML(self, listNumber, str):
@@ -140,17 +143,6 @@ class md2html(object):
         with open(out_path, 'w', encoding='utf-8', errors='xmlcharrefreplace') as out_File:
             out_File.write(post_html_content)
         print('使用线程%s生成文章：《%s》,其标签为：%s，地址在：%s'%(str, self.arr[listNumber].title, self.arr[listNumber].tags, out_path))
-        
-    def parse_main(self, thread_str, str):    #线程调用函数
-        first = thread_str.first
-        last = thread_str.last
-        for i in range(first,last):
-            if i < 5:             #默认解析列表前5篇文章
-                self.parse_to_HTML(i, str)
-            elif os.path.isfile(self.HTML_url(self.arr[i].md_url) ):  #如果后面的存在就不解析，不存在就解析               
-                continue
-            else:
-                self.parse_to_HTML(i, str)
             
     def Create_archive(self):
         print('生成目录中')
@@ -158,7 +150,7 @@ class md2html(object):
         temp_first = self.archlink_Detail[1]
         page_nav_l = self.page_nav_l
         page_nav_r = self.page_nav_r
-        
+        posts_url = None
         if (self.post_num <= self.articleMax): #只有一页 a
             print("a")
             s1 = ''
@@ -170,12 +162,13 @@ class md2html(object):
                     s1 =  s1+a
                 else:
                     continue
-            with open('index.html','w',encoding='utf-8') as only_onepg:
+            posts_url = "index.html"
+            with open(posts_url,'w',encoding='utf-8') as only_onepg:
                 only_onepg.write(self.template_acrchive.replace('{{page_nav}}',s1)\
                 .replace('{{title}}',self.site_name)\
                 .replace('{{site-name}}',self.site_name)\
                 .replace('{{nav}}',''))                 
-
+            self.sitemap(posts_url)
         elif(self.pages == 1): #第一页没有pre b
             print("b")
             s1 = ''
@@ -187,12 +180,14 @@ class md2html(object):
                     s1 =  s1+a
                 else:
                     continue
-            with open('index.html','w',encoding='utf-8') as pg:
+            posts_url = "index.html"
+            with open(posts_url,'w',encoding='utf-8') as pg:
                 pg.write(self.template_acrchive\
                 .replace('{{page_nav}}',s1)\
                 .replace('{{title}}',self.site_name)\
                 .replace('{{site-name}}',self.site_name)\
                 .replace('{{nav}}',page_nav_r.replace('{{right-link}}','pages/'+str(self.pages+1)+'.html')))
+            self.sitemap(posts_url)
         elif(self.pages == 2 and (self.is_only2page != True) ): #由递归衰减而到的第2页 c
             print("c")
             s1 = ''
@@ -204,13 +199,14 @@ class md2html(object):
                     s1 =  s1+a
                 else:
                     continue
-            with open('pages/'+str(self.pages)+'.html','w',encoding='utf-8') as pg:
+            posts_url = "pages/" + str(self.pages)+".html"
+            with open(posts_url,'w',encoding='utf-8') as pg:
                 pg.write(self.template_acrchive\
                 .replace('{{page_nav}}',s1)\
                 .replace('{{title}}','第'+str(self.pages)+'页 | '+ self.site_name)\
                 .replace('{{site-name}}',self.site_name)\
                 .replace('{{nav}}', page_nav_l.replace('{{left-link}}','../index.html')+page_nav_r.replace('{{right-link}}',str(self.pages+1)+'.html')))
-                
+            self.sitemap(posts_url)    
         elif(self.is_only2page): #如果一开始就是只有2页便执行如下代码 d
             print("d")
             s1 = ''
@@ -222,13 +218,14 @@ class md2html(object):
                     s1 =  s1+a
                 else:
                     continue
-            with open('pages/'+str(self.pages)+'.html','w',encoding='utf-8') as pg:
+            posts_url = "pages/" + str(self.pages)+".html"
+            with open(posts_url,'w',encoding='utf-8') as pg:
                 pg.write(self.template_acrchive\
                 .replace('{{page_nav}}',s1)\
                 .replace('{{title}}','第'+str(self.pages)+'页 | '+ self.site_name)\
                 .replace('{{site-name}}',self.site_name)\
                 .replace('{{nav}}', page_nav_l.replace('{{left-link}}','../index.html')))
-
+            self.sitemap(posts_url)
         elif(self.pages*self.articleMax >= self.post_num and ((self.pages-1)*self.articleMax <= self.post_num ) ): #最后一页 e
             print("e")
             s1 = '' 
@@ -240,12 +237,14 @@ class md2html(object):
                     s1 =  s1+a
                 else:
                     continue
-            with open('pages/'+str(self.pages)+'.html','w',encoding='utf-8') as pg:
+            posts_url = "pages/" + str(self.pages)+".html"
+            with open(posts_url,'w',encoding='utf-8') as pg:
                 pg.write(self.template_acrchive\
                 .replace('{{page_nav}}',s1)\
                 .replace('{{title}}','第'+str(self.pages)+'页 | '+ self.site_name)\
                 .replace('{{site-name}}',self.site_name)\
                 .replace('{{nav}}', page_nav_l.replace('{{left-link}}',str(self.pages-1)+'.html')))
+            self.sitemap(posts_url) 
         else:  #一般页面 f
             print("f")
             s1 = ''
@@ -264,79 +263,121 @@ class md2html(object):
                 .replace('{{site-name}}',self.site_name)\
                 .replace('{{nav}}', page_nav_l.replace('{{left-link}}',str(self.pages-1)+'.html')+page_nav_r.replace('{{right-link}}',\
                 str(self.pages+1)+'.html')))
+            self.sitemap("pages/" + str(self.pages)+".html")
         self.pages -=1
         if self.pages >0:
             self.Create_archive()
 
-    def preFunc(self):  #预先处理
-        '''预先判定这几个文件夹是否存在： source  pages   template   '''
-        if os.path.isdir(self.base) and os.path.isdir("pages") and os.path.isdir("template"):
-            '''判断这几个模板文件是否存在'''
-            if os.path.isfile('template/post_tem.html') and os.path.isfile('template/archive_tem.html') and os.path.isfile('template/archlink_Detail.html'):
-                with open('template/post_tem.html','r',encoding='utf-8') as aa:
-                    self.template_post = aa.read()
-                with open('template/archive_tem.html','r',encoding='utf-8') as bb:
-                    self.template_acrchive= bb.read()
-                with open('template/archlink_Detail.html','r',encoding='utf-8') as cc:
-                    archlink_Detail_data = cc.read()
-                self.archlink_Detail = re.compile('archive_post:(.*?)\n', re.S).findall(archlink_Detail_data) # return list
-                self.page_nav_l = re.search('page_nav_l:(.*?)\n', archlink_Detail_data, re.S).group(1)
-                self.page_nav_r = re.search('page_nav_r:(.*?)\n', archlink_Detail_data, re.S).group(1)
-            else:
-                print("模板文件夹里面欠缺必要的文件！")
-                return False 
-            for md in self.findAllFile():                
-                parent_dir = (os.path.dirname(md) ).replace("source", "posts")  # 不换就是：source   source/gongkao_zhuanlan
-                if os.path.isdir(parent_dir):
-                    pass
-                else:
-                    os.mkdir(parent_dir)
-                self.GetmdDetail(md)
-            self.post_num = len(self.arr)
-            #排序
-            if self.post_num >=2:
-                for i in range(1, self.post_num):#这里不需要-1，因为下面已经有j = i-1，否则在列表最后一个排不到
-                    key = self.arr[i] 
-                    j = i-1 
-                    while j>=0 and key.date.replace("-", "").replace(":", "").replace(" ", "") > self.arr[j].date.replace("-", "").replace(":", "").replace(" ", ""):
-                        self.arr[j+1] = self.arr[j]
-                        j-=1
-                    self.arr[j+1] = key
-            #生成sitemap.txt文件
-            self.sitemap()
-            
-            #置顶
-            topArr = []
-            top = self.post_num-1
-            while top>=0:
-                if(self.arr[top].topping.replace(" ", "") == "Yes"):
-                    topArr.insert(0,self.arr[top])    #插入到列表topArr中
-                    del (self.arr[top])               #删除列表arr中该元素
-                top -= 1    
-            self.arr = topArr + self.arr              #两个列表合并
-            del topArr                                #删除列表topArr
-            #self.PrintArr()
-
-            self.pages = (self.post_num//(self.articleMax+1)) + 1
-            print('博客导航有%s页，每页有%s篇文章（含隐藏页），总共有%s篇文章。'%(self.pages,self.articleMax, self.post_num))
-            if(self.pages == 2):
-                self.is_only2page = True      #判断一开始就是不是只有两个页面
-            return True                    #正常运行时，返回True
+    def sitemap(self, URL=None): #生成站点地图sitemap.txt
+        if URL == None:
+            sitemapstring = ""
+            for i in self.arr:
+                sitemapstring = sitemapstring + self.site_url +"/"+self.HTML_url(i.md_url) + "\n"
+            try:
+                with open("sitemap.txt", "w",encoding="utf-8") as sitemap:
+                    sitemap.write(sitemapstring)
+                sitemap.close()
+            except:
+                return False
         else:
-            print("请仔细检查source、pages、template这三个文件夹是否存在！")
-            return False
+            try:
+                with open("sitemap.txt", "a",encoding="utf-8") as sitemap:
+                    sitemap.write(self.site_url + "/"+URL + "\n")
+                sitemap.close()
+            except:
+                return False
+        return True
 
-    def sitemap(self):
-        sitemapstring = ""
-        for i in self.arr:
-            sitemapstring = sitemapstring + "http://blog.jtjiang.top/posts/"+self.HTML_url(i.md_url) + "\n"
-        try:
-            with open("sitemap.txt", "w",encoding="utf-8") as sitemap:
-                sitemap.write(sitemapstring)
-            sitemap.close()
-        except:
-            return False
+    def parse_main(self, thread_str, str):    #线程调用函数
+        first = thread_str.first
+        last = thread_str.last
+        for i in range(first,last):
+            if i < 5:             #默认解析列表前5篇文章
+                self.parse_to_HTML(i, str)
+            elif os.path.isfile(self.HTML_url(self.arr[i].md_url) ):  #如果后面的存在就不解析，不存在就解析               
+                continue
+            else:
+                self.parse_to_HTML(i, str)  
 
+    def preFunc(self):  #预先处理
+        '''预先判定这几个文件夹是否存在： source  pages   template   '''            
+        if os.path.isdir(self.base):
+            pass
+        else:
+            os.mkdir(self.base)
+            print(self.base + "文件夹不存在，已帮忙创建！")
+            return False
+        if os.path.isdir("pages"):
+            pass
+        else:
+            os.mkdir("pages")
+            print("pages文件夹不存在，已帮忙创建！")
+            return False
+        if os.path.isdir("template"):
+            pass
+        else:
+            os.mkdir("template")
+            print("template文件夹不存在，已帮忙创建！")
+            return False
+        if os.path.isdir("posts"):
+            pass
+        else:
+            os.mkdir("posts")
+            print("posts文件夹不存在，已创建！")
+            return False        
+        if os.path.isfile('template/post_tem.html') and os.path.isfile('template/archive_tem.html') and os.path.isfile('template/archlink_Detail.html'):
+            with open('template/post_tem.html','r',encoding='utf-8') as aa:
+                self.template_post = aa.read()
+            with open('template/archive_tem.html','r',encoding='utf-8') as bb:
+                self.template_acrchive= bb.read()
+            with open('template/archlink_Detail.html','r',encoding='utf-8') as cc:
+                archlink_Detail_data = cc.read()
+            self.archlink_Detail = re.compile('archive_post:(.*?)\n', re.S).findall(archlink_Detail_data) # return list
+            self.page_nav_l = re.search('page_nav_l:(.*?)\n', archlink_Detail_data, re.S).group(1)
+            self.page_nav_r = re.search('page_nav_r:(.*?)\n', archlink_Detail_data, re.S).group(1)
+        else:
+            print("模板文件夹里面欠缺必要的文件！\n 请参考：https://github.com/qing2zijin/staticblog 进行搭建")
+            return False 
+            
+        for md in self.findAllFile():                
+            parent_dir = (os.path.dirname(md) ).replace(self.base, "posts")  # 不换就是：source   source/gongkao_zhuanlan
+            if parent_dir == "posts":
+                pass
+            elif os.path.isdir(parent_dir):
+                pass
+            else:
+                os.mkdir(parent_dir)
+            self.GetmdDetail(md)
+        self.post_num = len(self.arr)
+        #排序
+        if self.post_num >=2:
+            for i in range(1, self.post_num):#这里不需要-1，因为下面已经有j = i-1，否则在列表最后一个排不到
+                key = self.arr[i] 
+                j = i-1 
+                while j>=0 and key.date.replace("-", "").replace(":", "").replace(" ", "") > self.arr[j].date.replace("-", "").replace(":", "").replace(" ", ""):
+                    self.arr[j+1] = self.arr[j]
+                    j-=1
+                self.arr[j+1] = key
+        
+        self.sitemap() #生成sitemap.txt文件
+        
+        #置顶
+        topArr = []
+        top = self.post_num-1
+        while top>=0:
+            if(self.arr[top].topping.replace(" ", "") == "Yes"):
+                topArr.insert(0,self.arr[top])    #插入到列表topArr中
+                del (self.arr[top])               #删除列表arr中该元素
+            top -= 1    
+        self.arr = topArr + self.arr              #两个列表合并
+        del topArr                                #删除列表topArr
+        #self.PrintArr()
+
+        self.pages = (self.post_num//(self.articleMax+1)) + 1
+        print('博客导航有%s页，每页有%s篇文章（含隐藏页），总共有%s篇文章。'%(self.pages,self.articleMax, self.post_num))
+        if(self.pages == 2):
+            self.is_only2page = True      #判断一开始就是不是只有两个页面
+        return True                    #正常运行时，返回True
     def Main(self):
         if(self.preFunc()): 
             lr = self.post_num 
@@ -363,9 +404,9 @@ class md2html(object):
             
 if __name__ == '__main__':
     start = time.time()
-    print('欢迎使用基于python的静态博客生成器\n现在开始生成页面了。\n')
+    print('欢迎使用基于python的静态博客生成器，\n项目地址：https://github.com/qing2zijin/staticblog \n现在开始生成页面了。\n')
     site_name = '月牙博客'
-    a = md2html('source/', site_name, "http://blog.jtjiang.top") #创建对象
+    a = md2html("source", site_name, "http://blog.jtjiang.top") #创建对象
     a.Main()  #调用对象内的函数
     del a
     end = time.time()
